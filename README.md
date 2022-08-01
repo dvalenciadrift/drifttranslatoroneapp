ead4458fdd8b6102b8999f977349697465faea93
David Valencia <dvalencia@drift.com>
2022-08-01 09:37:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Drift Translator App / One App Version
+
+Server side application written in Node that listens for message events and translates them according to the last detected language written by the user.
+Based on [this original repo](https://github.com/davehthompson/TranslateDriftChat) by [Dave Thompson](https://github.com/davehthompson)
+
+### Application Logic
+
+When a site visitor engages with a bot in a specific language the application will detect the language, listen for any messages sent by the site visitor, translate them, and write an internal note inside of the conversation to the target language (English in this example). Additionally, when a chat agent writes back to the chat the application will translate the message into the visitors identified language. 
+
+### Installation and Setup
+---
+There are _three_ core components of this application
+* Access to Google's Translation API
+* One Drift Developer Application for listening for specific message events (```new_message``` and ```new_command_message```)
+* Node.js runtime with appropriate dependencies executing serverside logic
+
+#### Google Translation API
+Before you can start using the Cloud Translation API, you must have a project that has the Cloud Translation API enabled, and you must have a private key with the appropriate credentials. You can also install client libraries for common programming languages to help you make calls to the API. This [setup page](https://cloud.google.com/translate/docs/setup) will walk you through getting it setup properly.
+
+#### Drift Developer Applications
+In order for Drift to expose data to the application we need to create one drift developer applications with the appropriate scopes. [This guide](https://devdocs.drift.com/docs/quick-start) will walk you through setting an dev application up. You need to subscribe to the ```new_message``` and ```new_command_message``` webhooks.
+
+#### Node and core dependencies
+
+This project leverages core libraries/dependencies that are listed below:
+
+* @google-cloud/translate (https://cloud.google.com/translate/docs/reference/libraries/v2/nodejs)
+* body-parser (https://www.npmjs.com/package/body-parser)
+* dotenv (https://www.npmjs.com/package/dotenv)
+* express (https://www.npmjs.com/package/express)
+* ngrok (https://www.npmjs.com/package/ngrok)
+* node (https://nodejs.org/en/)
+* npm (https://www.npmjs.com/package/npm)
+* superagent (https://www.npmjs.com/package/superagent)
+
+Install Node.js first (npm should be included) locally or in the cloud depending on where you are developing. Then simply clone/download the repo locally and install the relevant dependences listed. For a one line command to install the dependencies see below:
+
+```
+npm i @google-cloud/translate body-parser dotenv express ngrok superagent
+```
+
+Once installed navigate to the root directoy where the files resides and run the following command in the IDE of your choice 
+```
+node index.js
+```
+
+### Considerations
+---
+#### Drift Developer Application
+
+In order for the experience to function properly you will need to set up a developer app inside of your drift account for Drift to POST relevant information to your application. Please ensure you are leveraging proper scopes/subscribing to relevant webhooks. [This guide will help get this set up for you](https://devdocs.drift.com/docs/quick-start).
+
+##### HTTP POST Requests
+This application leverages `ngrok` as it's means to listening and digesting events from Drift. It is being leveraged in a free capacity and therefore whenever the service restarts a new randomly generated URL will be exposed to recieve POST requests from Drift. This URL will need to be updated inside of the relevant Drift Dev Application in order to function properly after each restart -- as seen below: 
+
+![Capture](https://user-images.githubusercontent.com/57994411/151228007-563fafb8-e7e2-438c-98a5-81537987e4e6.JPG)
+## Demo of Site Visitor Experience (German)
+![DRIFT-VIDEO-3045136-3149711-1643082412](https://user-images.githubusercontent.com/57994411/150907452-797a7a1c-af96-494c-ab3f-7843565143fa.gif)
+
+## Demo of Chat Agent Experience (German --> English & English --> German)
+
+![DRIFT-VIDEO-3047278-3151853-1643167646](https://user-images.githubusercontent.com/57994411/151099191-2b65e93a-e250-46e0-bf1a-876b76fc2dac.gif)
+
---
+++ index.js
@@ -0,0 +1,144 @@
+//Declare dependencies
+require('dotenv').config()
+const { Translate } = require('@google-cloud/translate').v2;
+const ngrok = require('ngrok')
+const express = require('express')
+const request = require('superagent')
+const bodyParser = require('body-parser')
+
+//Define Variables
+const app = express()
+const port = process.env.PORT || 4040
+const conversationApiBase = 'https://driftapi.com/conversations/'
+const oneAppToken = process.env.ONEAPP_TOKEN
+const projectId = process.env.PROJECT_ID
+const keyFilename = 'keyfile.json'
+const translate = new Translate({ projectId, keyFilename });
+
+//Global Variable to store current detected language
+let detectedText
+
+//leverage middleware for response/request objects
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//serve server locally
+app.listen(port, () => {
+    console.log(process.env.USER_ID)
+    console.log(`App running locally on: http://localhost:${port}`);
+});
+//expose local webserver to internet
+startNgrok = async () => {
+    const url = await ngrok.connect(port)
+    console.log(`Payload digestion for translating site visitor message URL is: ${url}/translate`)
+}
+startNgrok()
+
+//define overall logic for listening to Drift Events and taking translation action
+app.post('/translate', async (req, res) => {
+    if (req.body.data.type === 'private_note' && req.body.type === 'new_command_message') {
+        try {
+            let driftAuthorType = req.body.data.author.type
+            let driftAuthorId = req.body.data.author.id
+            let driftMessageType = req.body.data.type
+            let driftMessage = req.body.data.body
+            let validateNote = driftMessage.search('/translate')
+            if (driftAuthorType === 'user' && driftMessageType === 'private_note' && validateNote != -1) {
+                console.log('first if inside private note')
+                let driftAuthorId = req.body.data.author.id
+                let regex = /([/])\w+/g
+                let ModifiedDriftMessage = driftMessage.replace(regex, '')
+                console.log(`This is the ModifiedDriftMessage value: ${ModifiedDriftMessage}`)
+                console.log(`Translating ${ModifiedDriftMessage} as it was an internal note created by a Drift User`)
+                let driftConversationAgent = req.body.data.conversationId
+                console.log(`This is the value of driftConversationAgent: ${driftConversationAgent}`)
+                let translatedText = await translateText(ModifiedDriftMessage, detectedText)
+                console.log('before sendinfg translated message')
+                request.post(`${conversationApiBase}${driftConversationAgent}/messages`)
+                    .set('Content-type', 'application/json')
+                    .set('Authorization', `Bearer ${oneAppToken}`)
+                    .send({
+                        "type": "chat",
+                        "body": `${translatedText[0]}`,
+                        "userId": driftAuthorId
+                    })
+                    .then(res => {
+                        return res
+                    })
+                    .catch(err => {
+                        return {
+                            error: err.message
+                        }
+                    })
+            } else {
+                console.log('Internal note was not made by a user')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    } else if (req.body.data.type === 'chat' && req.body.data.author.type === 'contact') {
+        console.log('else if')
+        try {
+            let driftBot = req.body.data.author.bot
+            let driftAuthorType = req.body.data.author.type
+            if (driftAuthorType === 'contact') {
+                let driftMessage = req.body.data.body
+                let driftConversation = req.body.data.conversationId
+                console.log(`Translating ${driftMessage} as it was entered by a contact`)
+                detectedText = await detectLanguage(driftMessage)
+                console.log('detected language', detectedText)
+                let dbEntryRaw = {
+                    conversationId: driftConversation,
+                    langcode: detectedText[0].language
+                }
+                let translatedText = await translateText(driftMessage, 'en')
+                console.log(translatedText)
+                request.post(`${conversationApiBase}${driftConversation}/messages`)
+                    .set('Content-type', 'application/json')
+                    .set('Authorization', `Bearer ${oneAppToken}`)
+                    .send({
+                        "type": "private_note",
+                        "body": `${driftMessage} *** Translates in English to: *** ${translatedText[0]}`
+                    })
+                    .then(res => {
+                        return res
+                    })
+                    .catch(err => {
+                        return {
+                            error: err.message
+                        }
+                    })
+            } else {
+                console.log(`Did not process message as it was not a human writing directly into the chat`)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+})
+
+//Helper Functions
+
+//Detect language
+const detectLanguage = async (driftMessage) => {
+    // Run request
+    const response = await translate.detect(driftMessage);
+
+    return response[0].language
+}
+
+//Translate text 
+const translateText = async (driftMessage, detectedText) => {
+    const toTranslate = driftMessage
+    const targetLang = 'en'
+    // Run request
+    const response = await translate.translate(toTranslate, detectedText);
+    return response
+
+}
+
+//Search db of Convo ID's and associated language code
+const findLang = (lang) => {
+    return lang.conversationId === driftConversationAgent
+}
+
